ed279c92ca69737bda06648d44497e459fef3dc2
sharath <sharath@datahut.co>
2023-08-09 23:57:52 +0530

    home.js text edited

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -16,40 +16,35 @@ function Home2() {
         <Row>
           <Col md={8} className="home-about-description">
             <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+              LET ME <span className="purple"> INTRODUCE </span> OUR SELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              <h3>
+            <b className="purple">Step into the future with </b>
+            Nalara Tech, 
+            </h3>
               <br />
-              <br />I am fluent in classics like
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple">your premier digital services partner. From web innovation to tailored solutions, we fuse technology and creativity to amplify your brand's impact. Join us and redefine what's possible in the digital realm. Your success, our expertise.</b>
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+               &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain.
-                </b>
+                <b className="purple">Elevate your online presence, streamline operations, and conquer new horizons alongside a team that's dedicated to your digital triumph.  </b>  Welcome to limitless possibilities. {" "}
+  
               </i>
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              Welcome to <b className="purple">Nalara</b>
               <i>
                 <b className="purple">
                   {" "}
-                  Modern Javascript Library and Frameworks
+                  Tech.
                 </b>
               </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+              &nbsp; 
+        
             </p>
           </Col>
           <Col md={4} className="myAvtar">
