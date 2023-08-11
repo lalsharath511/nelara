a9f83c4d4641d5b5e35e4fe6e0a0cd6b1be893f5
sharath <sharath@datahut.co>
2023-08-12 02:18:46 +0530

    email changed in contact page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -79,7 +79,7 @@ const Footer = () => {
                                         </Link>
                                     </li>
                                     <li className="ml-4">
-                                        <Link to="#" className="flex justify-center items-center text-blue-900 hover:text-gray-500 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Facebook">
+                                        <Link to="https://www.facebook.com/profile.php?id=61550116136449" className="flex justify-center items-center text-blue-900 hover:text-gray-500 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Facebook">
                                         <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M14.023 24L14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257z" />
                                         </svg>
