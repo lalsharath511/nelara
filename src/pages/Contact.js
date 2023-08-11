756b42541bcaa9bd1543ea5f56fa524733617cdb
sharath <sharath@datahut.co>
2023-08-11 21:56:57 +0530

    email changed in contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 import Notiflix from 'notiflix';
 
 const Contact = () => {
-    useDocTitle('MLD | Molad e Konsult - Send us a message')
+    useDocTitle('Nalara Tech |Contacts  - Send us a message')
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
