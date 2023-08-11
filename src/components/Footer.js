a12c63604bff63421f36ed64741908a63c81a11e
sharath <sharath@datahut.co>
2023-08-12 02:48:05 +0530

    email changed in contact page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -69,10 +69,12 @@ const Footer = () => {
                                 <div className="text-md font-medium mb-6">
                                     Follow us on social media.
                                 </div>
+                     <script src="https://cdn.botpress.cloud/webchat/v0/inject.js"></script>
+                    <script src="https://mediafiles.botpress.cloud/3203c1ee-8671-45e9-91a3-037e9e5c8f28/webchat/config.js" defer></script>
                         <div className="mx-auto text-center mt-2">
                                 <ul className="flex justify-center mb-4 md:mb-0">
                                     <li>
-                                        <Link to="#" className="flex justify-center items-center text-blue-900 hover:text-gray-500 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Twitter">
+                                        <Link to="https://mediafiles.botpress.cloud/3203c1ee-8671-45e9-91a3-037e9e5c8f28/webchat/bot.html" className="flex justify-center items-center text-blue-900 hover:text-gray-500 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Twitter">
                                         <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M24 11.5c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4 0 1.6 1.1 2.9 2.6 3.2-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H8c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4c.7-.5 1.3-1.1 1.7-1.8z" />
                                         </svg>
@@ -90,8 +92,7 @@ const Footer = () => {
                     </div>          
 
                     </div>
-                    <script src="https://cdn.botpress.cloud/webchat/v0/inject.js"></script>
-                    <script src="https://mediafiles.botpress.cloud/3203c1ee-8671-45e9-91a3-037e9e5c8f28/webchat/config.js" defer></script>
+
                  
 
                     <div className="flex flex-wrap items-center md:justify-between justify-center mx-auto px-4">
